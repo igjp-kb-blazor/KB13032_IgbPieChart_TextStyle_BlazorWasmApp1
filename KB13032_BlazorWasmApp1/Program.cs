b2056fd91a2065a45aaf7caefc5cf733dc295bdf
norikois <33947244+norikois@users.noreply.github.com>
2024-02-29 15:13:50 +0900

    初回コミット

---
+++ KB13032_BlazorWasmApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using IgniteUI.Blazor.Controls;
+using KB13032_BlazorWasmApp1;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIgniteUIBlazor(
+    typeof(IgbItemLegendModule),
+    typeof(IgbPieChartModule)
+);
+
+await builder.Build().RunAsync();
